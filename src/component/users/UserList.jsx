83428458b55cc4df64662c0c232e1448fdde7556
Karlo Braovac <karlo.braovac@gmail.com>
2022-03-01 14:01:12 +0100

    UserList Component

--- src/component/users/UserList.jsx
+++ src/component/users/UserList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserList() {
   const [users, setUsers] = useState();
@@ -28,8 +29,8 @@ function UserList() {
     <Spinner />
   ) : (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user, index) => (
-        <h3 key={index}>{user.login}</h3>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
