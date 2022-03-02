a587fff30297f098d7b9996f8b93e4ccbbf74bf4
Karlo Braovac <karlo.braovac@gmail.com>
2022-03-02 13:25:09 +0100

    User Search Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import UserList from "../component/users/UserList";
+import UsersSearch from "../component/users/UsersSearch";
 
 function Home() {
   return (
     <>
+      <UsersSearch />
       <UserList />
     </>
   );
